8e718978d0ff3f652bb436e217f7c48382102675
LeonMarchetti <mleonardoa@gmail.com>
2019-03-29 16:35:40 -0300

    Preparando para usar componente mapa

--- www/js/mapa.js
+++ www/js/mapa.js
@@ -2,7 +2,7 @@ POPUP_CONFIG = { autoPan: false };
 
 Vue.component("mapa", {
     template: "#templateMapa",
-    props: ["alarmas", "pos_inicial"],
+    props: ["alarmas", "latlng", "pos_inicial",],
     data: function() { 
         return {
             mapa:               null,
@@ -39,7 +39,8 @@ Vue.component("mapa", {
         },
         initMarcadores: function() {
             // Popup
-            var popup_actual = L.popup(POPUP_CONFIG).setContent("<b>Guardar</b>");
+            var popup_actual = L.popup(POPUP_CONFIG)
+                .setContent("<span class='spanPopup'>Guardar</span>");
             
             // Marcador actual
             this.marcador_actual = L.marker([0, 0])
@@ -53,50 +54,47 @@ Vue.component("mapa", {
         onClickPopup: function() {
             var pos_actual = this.marcador_actual.getLatLng();
             console.log(`Guardar: ${pos_actual}`);
-        },
-        onLocationFound: function(e) {
-            this.marcador_actual.setLatLng(e.latlng);
+            
+            // TODO: Recibir evento en el componente raiz para guardar una alarma.
+            // Tengo que pasar la latitud y la longitud.
+            this.$emit("guardar", [pos_actual.lat, pos_actual.lng]);
         },
         // Marcadores:
-        mostrarMarcadores: function() {
-            this.borrarMarcadores();
-            
-            for (var i = 0; i < this.alarmas.length; i++) {
-                var alarma = this.alarmas[i];
-                
-                // Ícono según si la alarma está activa o inactiva
-                var icono = (alarma.activo) ? 
-                    this.icono_activo : 
-                    this.icono_inactivo;
-                 
-                // Marcador
-                var popup = L.popup(POPUP_CONFIG)
-                    .setContent(alarma.descripcion);
-                var marcador = L.marker([alarma.latitud, alarma.longitud])
-                    .addTo(this.mapa)
-                    .setIcon(icono)
-                    .bindPopup(popup);
-                    
-                // Círculo alrededor del marcador (No funciona)
-                var circulo = L.circle([alarma.latitud, alarma.longitud], alarma.distancia)
-                    .addTo(this.mapa);
-                    
-                /* Guardar las referencias al marcador y al círculo para poder 
-                 * borrarlos después.
-                 */
-                this.marcadores.push(marcador);
-                this.circulos.push(circulo);
-            }
-        },
         borrarMarcadores: function() {
             while (this.marcadores.length) {
                 this.mapa.removeLayer(this.marcadores.pop());
                 this.mapa.removeLayer(this.circulos.pop());
             }
         },
+        dibujarMarcadores: function() {
+            // Se dibuja un marcador y un círculo para cada alarma en el mapa.
+            for(var i = 0; i < this.alarmas.length; i++) {
+                var alarma = this.alarmas[i];
+                var icono = (alarma.activo)? this.icono_activo : this.icono_inactivo;
+                    
+                var marcador = L.marker([alarma.latitud, alarma.longitud])
+                    .addTo(this.mapa)
+                    .setIcon(icono)
+                    .bindPopup(alarma.descripcion);
+                    
+                var circulo = L.circle([alarma.latitud, alarma.longitud], alarma.distancia)
+                    .addTo(this.mapa);
+                    
+                this.marcadores.push(marcador);
+                this.circulos.push(circulo);
+            }
+        },
         watch: {
             alarmas: function() {
-                this.mostrarMarcadores();
+                console.log("mapa.js - Cambio en alarmas detectado");
+                
+                this.borrarMarcadores();
+                this.dibujarMarcadores();
+            },
+            latlng: function() {
+                console.log("mapa.js - Cambio en latlng detectado");
+                
+                this.marcador_actual.setLatLng(this.latlng[0], this.latlng[1]);
             },
         },
     },
